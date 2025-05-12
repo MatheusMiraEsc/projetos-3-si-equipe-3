cbd62a97a528b69ef73f7ad7b92ee03022fa1a32
Bruno <bomf@cesar.school>
2025-05-12 14:31:05 -0300

    add ValidadorDePecaTest test module

---
+++ src/test/java/ingressart/teatro/util/ValidadorDePecaTest.java
@@ -0,0 +1,69 @@
+package ingressart.teatro.util;
+
+import ingressart.teatro.model.Peca;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidadorDePecaTest {
+    
+    @Test
+    public void deveRetornarNullQuandoValida() {
+        Peca peca = new Peca();
+        peca.setNome("Hamlet");
+        peca.setDescricao("Tragédia clássica");
+        peca.setData(LocalDate.now().plusDays(1));
+        peca.setHora(LocalTime.of(20, 0));
+        peca.setValor_ingresso(50.0);
+    }
+
+    @Test
+    public void deveRetornarNomeVazio() {
+        Peca peca = new Peca();
+        peca.setNome("  ");
+        peca.setDescricao("Descrição qualquer");
+        peca.setData(LocalDate.now().plusDays(1));
+        peca.setHora(LocalTime.of(20,0));
+        peca.setValor_ingresso(50.0);
+
+        assertEquals("Nome é obrigatório", ValidadorDePeca.validar(peca));
+    }
+
+    @Test
+    public void deveDetectarDescricaoVazia() {
+        Peca peca = new Peca();
+        peca.setNome("Nome qualquer");
+        peca.setDescricao("  ");
+        peca.setData(LocalDate.now().plusDays(1));
+        peca.setHora(LocalTime.of(20, 0));
+        peca.setValor_ingresso(50.0);
+
+        assertEquals("Descrição é obrigatória", ValidadorDePeca.validar(peca));
+    }
+
+    @Test
+    public void deveDetectarDataNoPassado() {
+        Peca peca = new Peca();
+        peca.setNome("Nome qualquer");
+        peca.setDescricao("Descrição qualquer");
+        peca.setData(LocalDate.now().minusDays(1));
+        peca.setHora(LocalTime.of(20, 0));
+        peca.setValor_ingresso(50.0);
+        
+        assertEquals("Data inválida", ValidadorDePeca.validar(peca));
+    }
+
+    @Test
+    public void deveDetectarValorNegativo() {
+        Peca peca = new Peca();
+        peca.setNome("Nome qualquer");
+        peca.setDescricao("Descrição qualquer");
+        peca.setData(LocalDate.now().plusDays(1));
+        peca.setHora(LocalTime.of(20, 0));
+        peca.setValor_ingresso(-10.0);
+
+        assertEquals("Valor inválido", ValidadorDePeca.validar(peca));
+    }
+}
